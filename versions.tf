7aaebf98beed576295caabf4c83bbeda2957ce1e
Josh Luedeman <josh@joshluedeman.com>
2026-04-19 20:15:23 -0400

    fix: correct provider source to POps-Rox/azutils

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
       version = ">= 3.7.0, < 4.0"
     }
     popsrox-utils = {
-      source  = "POps-Rox/popsrox-utils"
+      source  = "POps-Rox/azutils"
       version = "~> 1.0.4"
     }
     random = {
